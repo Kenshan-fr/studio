504ee8d6d89aba79be54b2ff29cbfbd2beb92ca6
Thomas NOEL <t.noel02553@gmail.com>
2025-09-03 19:21:38 +0000

    Voici la clé Anon :eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,11 +3,11 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 // --- VOS CLÉS SUPABASE SONT À METTRE ICI ---
 // Allez dans votre projet Supabase > Project Settings > API pour les trouver.
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://srpdyatoxfdgdyahyewo.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "VOTRE_CLE_ANON_SUPABASE";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNycGR5YXRveGZkZ2R5YWh5ZXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MjYwMTksImV4cCI6MjA3MjUwMjAxOX0.mA5tyMhHCbjzZE-jBd__pift2jeoF7Cvf_8zn2umnhg";
 
 let supabase: SupabaseClient;
 
-if (supabaseUrl === "https://srpdyatoxfdgdyahyewo.supabase.co" || supabaseAnonKey === "VOTRE_CLE_ANON_SUPABASE") {
+if (supabaseUrl === "VOTRE_URL_SUPABASE" || supabaseAnonKey === "VOTRE_CLE_ANON_SUPABASE") {
     console.warn("****************************************************************");
     console.warn("ATTENTION: Les clés Supabase ne sont pas configurées.");
     console.warn("Veuillez mettre à jour le fichier src/lib/supabase.ts");
