8a8a83da33d4884869cb9ad11ce31913e7f002df
Thomas NOEL <t.noel02553@gmail.com>
2025-09-03 18:29:32 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,13 @@ import { getStorage } from "firebase/storage";
 // TODO: Replace the following placeholder values with your new Firebase project's configuration.
 // You can find this configuration in your Firebase project settings under "General".
 const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY_ICI", // Replace with your actual API Key
-  authDomain: "VOTRE_AUTH_DOMAIN_ICI", // e.g., votre-projet.firebaseapp.com
-  projectId: "VOTRE_PROJECT_ID_ICI", // Replace with your actual Project ID
-  storageBucket: "VOTRE_STORAGE_BUCKET_ICI", // e.g., votre-projet.appspot.com
-  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID_ICI",
-  appId: "VOTRE_APP_ID_ICI"
+  "projectId": "surdice-rate",
+  "appId": "1:946797933867:web:148ae8a716bae2dd7f416e",
+  "storageBucket": "surdice-rate.appspot.com",
+  "apiKey": "AIzaSyCt3FZRSCEOqOPqHAYSL26hs_v7kmYaRfk",
+  "authDomain": "surdice-rate.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "946797933867"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
